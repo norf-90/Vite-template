5925a0212ce5a8b67592b347ff4c6d4121ea3fe0
Oleksandr Shmatko <blacknorfolk@gmail.com>
2023-04-21 13:47:17 +0300

    first deploy

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
-import { defineConfig } from 'vite';
+/* eslint-disable import/no-extraneous-dependencies */
 import react from '@vitejs/plugin-react';
+
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +13,5 @@ export default defineConfig({
       }
     })
   ],
-  base: '/vite-template'
+  base: '/Vite-template'
 });
