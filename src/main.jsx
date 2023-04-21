ab356005a9b5392bff0e2bab85169be4a982547a
Oleksandr Shmatko <blacknorfolk@gmail.com>
2023-04-21 15:11:32 +0300

    ready

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GlobalStyles } from './components/GlobalStyles.styled';
 
-import App from './App';
-import './index.css';
+import App from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <GlobalStyles />
   </React.StrictMode>
 );
